35d5379e8c8cb7c896dc722acd770e14dc4ff6bc
nataliadominguez99 <natalia.dominguez.work@gmail.com>
2025-08-05 18:00:20 +0200

    lab 2 day 2 week 3

--- solutions.sql
+++ solutions.sql
@@ -1,1 +1,52 @@
--- Add you solution queries below:
+-- 1. Write a query to display for each store its store ID, city, and country.
+SELECT s.store_id, cty.city, ctry.country
+FROM store AS s
+JOIN address AS a ON s.address_id = a.address_id
+JOIN city AS cty ON a.city_id = cty.city_id
+JOIN country AS ctry ON cty.country_id = ctry.country_id;
+
+-- 2. Write a query to display how much business, in dollars, each store brought in.
+SELECT store_id, store, total_sales
+FROM sales_by_store;
+
+-- 3. What is the average running time of films by category?
+SELECT category,length, ROUND(AVG("length"), 3)  AS "Average film length"
+FROM film_list
+GROUP BY category;
+
+-- 4. Which film categories are longest?
+SELECT category, MAX("length") AS longest_film_length
+FROM film_list
+GROUP BY category
+ORDER BY longest_film_length DESC;
+
+-- 5. Display the most frequently rented movies in descending order.
+SELECT f.title, COUNT(r.rental_id) AS rental_count
+FROM rental r
+JOIN inventory i ON r.inventory_id = i.inventory_id
+JOIN film f ON i.film_id = f.film_id
+GROUP BY f.title
+ORDER BY rental_count DESC;
+
+-- 6. List the top five genres in gross revenue in descending order.
+SELECT c.name AS category, SUM(p.amount) AS total_revenue
+FROM payment p
+JOIN rental r ON p.rental_id = r.rental_id
+JOIN inventory i ON r.inventory_id = i.inventory_id
+JOIN film f ON i.film_id = f.film_id
+JOIN film_category fc ON f.film_id = fc.film_id
+JOIN category c ON fc.category_id = c.category_id
+GROUP BY c.name
+ORDER BY total_revenue DESC
+LIMIT 5;
+
+-- 7. Is "Academy Dinosaur" available for rent from Store 1?
+SELECT i.inventory_id
+FROM inventory i
+JOIN film f ON i.film_id = f.film_id
+WHERE f.title = 'Academy Dinosaur'
+  AND i.store_id = 1
+  AND i.inventory_id NOT IN (
+      SELECT inventory_id
+      FROM rental
+      WHERE return_date IS NULL);
