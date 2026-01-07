34c461221f8a20ba0160031493f2a17d66d39e5b
gail-mar <gail.marechal@hotmail.be>
2026-01-07 17:58:35 +0100

    finished task

--- solutions.sql
+++ solutions.sql
@@ -1,1 +1,50 @@
--- Add you solution queries below:
+-- ASELECT s.store_id, ci.city, co.country
+FROM store s
+JOIN address a ON s.address_id = a.address_id
+JOIN city ci ON a.city_id = ci.city_id
+JOIN country co ON ci.country_id = co.country_id;
+dd
+
+ you solution queries below:
+SELECT s.store_id, SUM(p.amount) AS total_sales
+FROM payment p
+JOIN rental r ON p.rental_id = r.rental_id
+JOIN inventory i ON r.inventory_id = i.inventory_id
+JOIN store s ON i.store_id = s.store_id
+GROUP BY s.store_id;
+
+SELECT c.name AS category, ROUND(AVG(f.length), 2) AS avg_length
+FROM film f
+JOIN film_category fc ON f.film_id = fc.film_id
+JOIN category c ON fc.category_id = c.category_id
+GROUP BY c.name;
+
+SELECT c.name AS category, ROUND(AVG(f.length), 2) AS avg_length
+FROM film f
+JOIN film_category fc ON f.film_id = fc.film_id
+JOIN category c ON fc.category_id = c.category_id
+GROUP BY c.name
+ORDER BY avg_length DESC;
+
+SELECT f.title, COUNT(r.rental_id) AS rental_count
+FROM rental r
+JOIN inventory i ON r.inventory_id = i.inventory_id
+JOIN film f ON i.film_id = f.film_id
+GROUP BY f.title
+ORDER BY rental_count DESC;
+
+SELECT c.name AS category, ROUND(SUM(p.amount), 2) AS revenue
+FROM payment p
+JOIN rental r ON p.rental_id = r.rental_id
+JOIN inventory i ON r.inventory_id = i.inventory_id
+JOIN film f ON i.film_id = f.film_id
+JOIN film_category fc ON f.film_id = fc.film_id
+JOIN category c ON fc.category_id = c.category_id
+GROUP BY c.name
+ORDER BY revenue DESC
+LIMIT 5;
+
+SELECT f.title, i.store_id
+FROM film f
+JOIN inventory i ON f.film_id = i.film_id
+WHERE f.title = 'Academy Dinosaur' AND i.store_id = 1;
